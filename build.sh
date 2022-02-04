58348b7dda20e58ffbfc9c2e2829e4604529d969
David Woller <dbw0011@users.noreply.github.com>
2022-02-04 09:16:21 -0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CODE_VERSION=$1
+ACCESS_TOKEN=a178d7ee81df41e09cb63402355b2076
+
+echo "Building minified file and source map"
+
+node_modules/uglify-js2/bin/uglifyjs2 app.js --mangle --source-map app.min.map --source-map-include-sources 1 > app.min.js
+
+node_modules/uglify-js2/bin/uglifyjs2 app.min.js --mangle --in-source-map app.min.map --source-map app2.min.map --source-map-include-sources 1 > app2.min.js
+
+
+echo "Uploading source map for code version $CODE_VERSION"
+
+curl https://sourcemaps.rollbardev.com/api/1/sourcemap \
+  -F access_token=$ACCESS_TOKEN \
+  -F version=$CODE_VERSION \
+  -F minified_url=http://localhost:8080/app2.min.js \
+  -F source_map=@app2.min.map
