4871f4d055e3420b2646ce99fa1bb0766a571b4d
David Woller <dbw0011@users.noreply.github.com>
2022-02-08 11:48:45 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ ACCESS_TOKEN=30240509d1d04e069900225c024fa3f2
 
 echo "Building minified file and source map"
 
-node_modules/uglify-js2/bin/uglifyjs2 app.js --mangle --source-map app.min.map --source-map-include-sources 1 > app.min.js
+node_modules/uglify-js2/bin/uglifyjs2 app.js --mangle --source-map app.min.map --source-map-include-sources 1 -o app.min.js
 
-node_modules/uglify-js2/bin/uglifyjs2 app.min.js --mangle --in-source-map app.min.map --source-map app2.min.map --source-map-include-sources 1 > app2.min.js
+node_modules/uglify-js2/bin/uglifyjs2 app.min.js --mangle --in-source-map app.min.map --source-map app2.min.map --source-map-include-sources 1 -o app2.min.js
 
 echo "Uploading source map for code version $CODE_VERSION"
 
