c3d1b4efa8bbf22de285a50a829da2af18fb074a
David Woller <dbw0011@users.noreply.github.com>
2022-02-09 11:19:53 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Building minified file and source map"
 node_modules/uglify-js2/bin/uglifyjs2 app.js --mangle --source-map app.min.map --source-map-include-sources 1 -o app.min.js
 
 
-echo "Uploading source map for code version $CODE_VERSION"
+# echo "Uploading source map for code version $CODE_VERSION"
 
 #curl https://api.rollbar.com/api/1/sourcemap \
  # -F access_token=$ACCESS_TOKEN \
