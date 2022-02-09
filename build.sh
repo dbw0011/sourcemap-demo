4543b4ed6fd2a4109ccb1613a8349bd03e1f3bb7
dbw0011 <dwol4291@gmail.com>
2022-02-09 08:35:46 -0800

    change gitignore

--- build.sh
+++ build.sh
@@ -6,12 +6,11 @@ echo "Building minified file and source map"
 
 node_modules/uglify-js2/bin/uglifyjs2 app.js --mangle --source-map app.min.map --source-map-include-sources 1 -o app.min.js
 
-node_modules/uglify-js2/bin/uglifyjs2 app.min.js --mangle --in-source-map app.min.map --source-map app2.min.map --source-map-include-sources 1 -o app2.min.js
 
 echo "Uploading source map for code version $CODE_VERSION"
 
 curl https://api.rollbar.com/api/1/sourcemap \
   -F access_token=$ACCESS_TOKEN \
   -F version=$CODE_VERSION \
-  -F minified_url=http://localhost:8787/app2.min.js \
-  -F source_map=@app2.min.map
+  -F minified_url=http://localhost:8787/app.min.js \
+  -F source_map=@app.min.map
